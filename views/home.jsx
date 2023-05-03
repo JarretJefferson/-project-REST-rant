89b42f5b9e2e0f77962313df1179c5a72212b634
jarretjefferson <jayesper47@gmail.com>
2023-05-02 22:28:21 -0700

    added seed-comments and seed-places

--- views/home.jsx
+++ views/home.jsx
@@ -5,9 +5,12 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div>
-                  <img src="/images/meat-platter.jpg" alt="meat platter"/>
+                    <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                    <p>
+                        Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </p>
                 </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
@@ -18,4 +21,3 @@ function home () {
 }
 
 module.exports = home
-
